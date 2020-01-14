ac8e05e723cfe80dad6fedb89b905cc75f61676e
nleinone <nikoleino91@gmail.com>
2020-01-14 10:20:55 +0200

    Added button func to NewUserActivity

--- app/src/main/java/com/example/mc2020lab/newUserActivity.java
+++ app/src/main/java/com/example/mc2020lab/newUserActivity.java
@@ -2,7 +2,9 @@ package com.example.mc2020lab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewUserActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class NewUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+    }
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.newUserRegisterBtn) {
+            startActivity(new Intent(NewUserActivity.this, MainActivity.class));
+        }
     }
 }
