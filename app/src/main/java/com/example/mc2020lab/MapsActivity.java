a723745877db17ffb6c7babfbcf6d8a56492a32a
nleinone <nikoleino91@gmail.com>
2020-01-20 16:48:26 +0200

    map progress...

--- app/src/main/java/com/example/mc2020lab/MapsActivity.java
+++ app/src/main/java/com/example/mc2020lab/MapsActivity.java
@@ -1,11 +1,20 @@
 package com.example.mc2020lab;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,6 +30,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    public void getOwnCoordinates(int permission)
+    {
+        LocationRequest request = new LocationRequest();
+        request.setInterval(1000);
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+
+            FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+
+                        Location location = locationResult.getLastLocation();
+                        if (location != null) {
+                            //Save own coordinates to pref:
+                            double Longitude = location.getLongitude();
+                            double Latitude = location.getLatitude();
+
+                            String stringLongitude = Double.toString(Longitude);
+                            String stringLatitude = Double.toString(Latitude);
+
+                            SharedPreferences login_name_pref = getApplicationContext().getSharedPreferences("login_name", 0); // 0 - for private mode
+                            String loginName = login_name_pref.getString("LoginName", "No name");
+
+                            SharedPreferences pref = getApplicationContext().getSharedPreferences("shared_preference", 0); // 0 - for private mode
+
+                            pref.edit().putString(loginName + "_" + "longitude", stringLongitude).apply();
+                            pref.edit().putString(loginName + "_" + "latitude", stringLatitude).apply();
+                        }
+                    }
+            }, null);
+        }
+    }
+
     //REF: https://mobikul.com/picking-location-with-map-pin-using-google-maps-in-android/
     //REF: https://abhiandroid.com/programming/googlemaps
     //AIzaSyCLF1Na9WALrQOZ99j7OLzx3ubG0kwnpRM
@@ -32,6 +75,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     /**
@@ -47,13 +91,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        //Check permission
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
         //Get current location
+        getOwnCoordinates(permission);
+
+        SharedPreferences login_name_pref = getApplicationContext().getSharedPreferences("login_name", 0); // 0 - for private mode
+        String loginName = login_name_pref.getString("LoginName", "No name");
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("shared_preference", 0); // 0 - for private mode
+        String longitude = pref.getString(loginName + "_" + "longitude", "No longitude");
+        String latitude = pref.getString(loginName + "_" + "latitude", "No latitude");
+
+        Double doubleLongitude = Double.parseDouble(longitude);
+        Double doubleLatitude = Double.parseDouble(latitude);
+
         //Move camera to current location
         //REF: https://github.com/nleinone/CrowdSourcer/blob/master/app/src/main/java/com/example/nikol/growdsourcer/LocationTracker.java
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng current_location = new LatLng(doubleLatitude, doubleLongitude);
+        mMap.addMarker(new MarkerOptions().position(current_location).title("Current position marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current_location));
 
         //Marker drag:
         googleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
